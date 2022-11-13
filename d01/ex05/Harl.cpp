d45058312b0ce0b1141f95ed24d80984cb317758
genouf <gabriel.enouf@yahoo.fr>
2022-11-13 18:34:01 +0100

    [ADD] d01 ex05 v1

---
+++ d01/ex05/Harl.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: genouf <genouf@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 17:53:21 by genouf            #+#    #+#             */
+/*   Updated: 2022/11/13 18:32:36 by genouf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	return ;
+}
+
+Harl::~Harl()
+{
+	return ;
+}
+
+void	Harl::debug(void) const
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << std::endl;
+	return ;
+}
+
+void	Harl::info(void) const
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
+	return ;
+}
+
+void	Harl::warning(void) const
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	return ;
+}
+
+void	Harl::error(void) const
+{
+	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
+	return ;
+}
+
+void	Harl::complain(std::string level) const
+{
+	const std::string	tab_str[] = {"DEBUG", "INFO", "WARNING", "ERROR", ""};
+	const function_h	tab_f[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	int					i;
+	
+	i = 0;
+	while (!tab_str[i].empty() && level.compare(tab_str[i]) != 0)
+		i++;
+	if (i > 3)
+		std::cout << "Invalid complaint !" << std::endl;
+	else
+		(this->*(tab_f[i]))();
+	return ;
+}
---
+++ d01/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: genouf <genouf@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 17:50:37 by genouf            #+#    #+#             */
+/*   Updated: 2022/11/13 18:30:53 by genouf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl	harl;
+	
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("LOL");
+	return (0);
+}
