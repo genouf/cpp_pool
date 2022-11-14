3a1c360c62825d9a5faa31ad13160e0f7faf4ec4
genouf <gabriel.enouf@yahoo.fr>
2022-11-14 20:10:31 +0100

    [SAVE 42]

---
+++ d02/ex01/Fixed.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: genouf <genouf@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 09:52:00 by genouf            #+#    #+#             */
+/*   Updated: 2022/11/14 17:55:38 by genouf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+
+#include <iostream>
+
+class Fixed {
+
+public:
+	Fixed(void);
+	Fixed(Fixed const &src);
+	Fixed(const int nb);
+	Fixed(const float nb);
+	~Fixed();
+
+	/*						OPERATOR						*/
+	Fixed &		operator=(Fixed const & rhs);
+
+	/*						FUNCTIONS						*/
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+
+private:
+	int					_nbr;
+	static const int	_bits_tmp;
+
+};
+
+std::ostream &		operator<<(std::ostream & o, Fixed const & i);
+
+#endif
