eb92b9f216ff690f4c855fb4f6731bd9d7813406
genouf <gabriel.enouf@yahoo.fr>
2022-11-14 16:26:54 +0100

    [ADD] d02 ex00 v1

---
+++ d02/ex00/Fixed.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: genouf <genouf@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 09:51:58 by genouf            #+#    #+#             */
+/*   Updated: 2022/11/14 16:21:00 by genouf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+/*						CONSTRUCTOR						*/
+
+Fixed::Fixed(void) : _nbr(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+	return ;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return ;
+}
+
+/*						DESTRUCTOR						*/
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+/*						OPERATOR						*/
+
+Fixed &		Fixed::operator=(Fixed const & rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+		this->_nbr = rhs.getRawBits();
+	return *this;
+}
+
+std::ostream &		operator<<(std::ostream & o, Fixed const & i)
+{
+	std::cout << "The value of nbr is : " << i.getRawBits() << std::endl;
+	return o;
+}
+
+/*						FUNCTIONS						*/
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _nbr;
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_nbr = raw;
+	return ;
+}
+
+/*						STATIC						*/
+
+const int Fixed::_bits_tmp = 8;
---
+++ d02/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: genouf <genouf@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 09:52:03 by genouf            #+#    #+#             */
+/*   Updated: 2022/11/14 15:19:02 by genouf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	
+	return (0);
+}
