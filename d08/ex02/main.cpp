e12778674df0040c99adfdd40e0c49aef92627db
genouf <gabriel.enouf@yahoo.fr>
2022-11-30 19:14:30 +0100

    [ADD] new tests in d08 ex02

--- d08/ex02/main.cpp
+++ d08/ex02/main.cpp
@@ -6,41 +6,73 @@
 /*   By: genouf <genouf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 21:36:08 by genouf            #+#    #+#             */
-/*   Updated: 2022/11/29 14:28:53 by genouf           ###   ########.fr       */
+/*   Updated: 2022/11/30 19:06:42 by genouf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-
-	std::cout << mstack.top() << std::endl;
-
-	mstack.pop();
-
-	std::cout << mstack.size() << std::endl;
-
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-
-	MutantStack<int>::it it = mstack.begin();
-	MutantStack<int>::c_it ite = mstack.cend();
-
-	++it;
-	--it;
-	while (it != ite)
 	{
-		std::cout << *it << std::endl;
-		++it;
-	}
+		std::cout << "====== STACK ======" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
 
-	std::stack<int> s(mstack);
+		std::cout << mstack.top() << std::endl;
+
+		mstack.pop();
+
+		std::cout << "Size : " << mstack.size() << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+
+		MutantStack<int>::it it = mstack.begin();
+		MutantStack<int>::it ite = mstack.end();
+
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	{
+		std::cout << "====== LIST ======" << std::endl;
+		std::list<int> mlist;
+		mlist.push_back(5);
+		mlist.push_back(17);
+
+		std::cout << mlist.back() << std::endl;
+
+		mlist.pop_back();
+
+		std::cout  << "Size : " << mlist.size() << std::endl;
+
+		mlist.push_back(3);
+		mlist.push_back(5);
+		mlist.push_back(737);
+	//[...]
+		mlist.push_back(0);
+
+		std::list<int>::iterator it = mlist.begin();
+		std::list<int>::iterator ite = mlist.end();
+
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 	return (0);
 }
